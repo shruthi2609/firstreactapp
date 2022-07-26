29ddbf731a8080066ae9655e41aacc729ad0bc78
shruthi093 <jayasruthi.l@gmail.com>
2022-07-26 15:42:32 +0530

    JSX - React Day 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const tech="React JS"
+const lnk='https://www.google.com'
+let show=false
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+show&&<>
+<h1>Hello {tech}</h1>
+<p>para</p>
+<a href={lnk}>link</a>
+</>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
