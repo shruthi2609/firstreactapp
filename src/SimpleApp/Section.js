dde69c6f26b40cacb3cb39425dce739f67969813
shruthi093 <jayasruthi.l@gmail.com>
2022-07-27 10:23:43 +0530

    Components , imports and exports

---
+++ src/SimpleApp/Section.js
@@ -0,0 +1,27 @@
+const SectionGallery=()=>{
+    return(
+        <div>
+            <h1>Gallery</h1>
+        </div>
+    )
+}
+function SectionAchievement(){
+return(
+    <div>
+        <h1>Achievements Report</h1>
+    </div>
+)
+}
+
+function SectionFooter(){
+    return(
+        <div>
+            <h3>Copyrights @ xyz.com </h3>
+        </div>
+    )
+}
+
+export default SectionGallery;
+
+export {SectionFooter,SectionAchievement}
+
