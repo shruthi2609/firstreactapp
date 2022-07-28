c4a6b6085448c141110b60bb723fa15af80dfde1
shruthi093 <jayasruthi.l@gmail.com>
2022-07-28 10:52:15 +0530

    tasks day 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SimpleStateComponent from './StatesDemo/SimpleComponent';
+import StateEgOne from './StatesDemo/StateEgOne';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 <>
-<SimpleStateComponent></SimpleStateComponent>
+<StateEgOne></StateEgOne>
 
 </>
 );
