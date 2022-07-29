fa400045dc8d477b17d26c06e61625aa692b0153
shruthi093 <jayasruthi.l@gmail.com>
2022-07-29 10:51:14 +0530

    Update

--- src/LifeCycleMethods/LifeCycleMethods.js
+++ src/LifeCycleMethods/LifeCycleMethods.js
@@ -7,16 +7,24 @@ class LifeCycleMethods extends React.Component{
             count:0
         }
     }
-    static getDerivedStateFromProps(props,state){
+   /* static getDerivedStateFromProps(props,state){
         console.log("inside DS",props)
         return {msg:props.initialMsg};
+    }*/
+    shouldComponentUpdate(newProps,newState){
+        if(this.state.count===newState.count){
+            return false
+        }
+        else{
+            return true
+        }
     }
-
-   componentDidMount(){
-        console.log("did mount")
-        this.setState({count:this.state.count+1})//setstate is triggered
-    }
-   
+   increment=()=>{
+    this.setState({count:this.state.count+1})
+   }
+   componentDidUpdate(){
+    console.log("re render")
+   }
     render(){
         console.log('inside render')
         return(
@@ -24,7 +32,7 @@ class LifeCycleMethods extends React.Component{
                 <h1>LifeCycleMethods</h1>
                 <h2>{this.state.msg}</h2>
                 <h3> no of times rendered {this.state.count }</h3>
-                <button>view</button>
+                <button onClick={this.increment}>+</button>
             </div>
         )
     }
