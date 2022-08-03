3f934a70cdde8885547f3acd4a07b17dca8caa8a
shruthi093 <jayasruthi.l@gmail.com>
2022-08-03 15:43:19 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import ImageSearch from "./APICalls/ImageSearch";
-
-
-
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
+import ImageSearch from './APICalls/ImageSearch'
+import BlogSearch from "./APICalls/BlogSearch"
 function App(){
   return(
     <div>
-
-    <ImageSearch></ImageSearch>
-
- 
+      <BrowserRouter>
+      <nav>
+      <Link to='/image'>Image Search</Link>
+      <Link to='/search'>Contact Manager</Link>
+      </nav>
+      <Routes>
+        <Route path='/image' element={<ImageSearch></ImageSearch>}></Route>
+        <Route path='/search' element={<BlogSearch></BlogSearch>}></Route>
+      </Routes>
+      </BrowserRouter>
+   
     </div>
   )
 }
