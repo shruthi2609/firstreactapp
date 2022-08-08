d12c60ce5b91564552b045ca08f7c57eda7337fe
shruthi093 <jayasruthi.l@gmail.com>
2022-08-08 15:17:58 +0530

    navlink demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {BrowserRouter,Route,Routes,Link} from 'react-router-dom'
+import {BrowserRouter,Route,Routes,Link, NavLink} from 'react-router-dom'
 import Dashboard from "./NestedRoutes/DashBoard";
 import Login from "./NestedRoutes/Login";
+import PageNotFound from "./NestedRoutes/PageNotFound";
 import UserPage from "./NestedRoutes/UserPage";
 
 
@@ -9,12 +10,39 @@ function App(){
   return(
     <div>
       <BrowserRouter>
+    { /* <NavLink to='/user/login' style={({isActive})=>(
+        {color:isActive?'red':'blue'}
+      )}>Login
+        
+      </NavLink>
+      <NavLink to='/user/dashboard' style={({isActive})=>(
+        {color:isActive?'red':'blue'}
+      )}>Dashboard
+        
+      </NavLink>*/}
+      <NavLink to='/user/login'>
+      {
+        ({isActive})=>(
+          <button className={isActive?'btn-primary':'btn-danger'}>login</button>
+        )
+      }
+      </NavLink>
+      <NavLink to='/user/dashboard'>
+      {
+        ({isActive})=>(
+          <button className={isActive?'btn-primary':'btn-danger'}>Dashboard</button>
+        )
+      }
+      </NavLink>
         <Routes>
         <Route path='/user' element={<UserPage></UserPage>}>
-          <Route path='login' element={<Login></Login>}>
+          <Route  path='login' element={<Login></Login>}>
           </Route>
-          <Route path='dashboard' element={<Dashboard></Dashboard>}></Route>
+          <Route  path='dashboard' element={<Dashboard></Dashboard>}></Route>
+          <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
         </Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/NestedRoutes/PageNotFound.js
@@ -0,0 +1,6 @@
+function PageNotFound(){
+    return(
+        <h1>Page not found</h1>
+    )
+}
+export default PageNotFound
